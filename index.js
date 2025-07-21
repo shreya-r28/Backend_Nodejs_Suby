e3083cf1fdf0b816f0389f855aca99cc0fbf9119
shreya-r28 <shreyareddyguduru@gmail.com>
2025-07-21 11:07:58 +0530

    added path module

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const path=require('path')
 const app=express();
 const PORT=process.env.PORT || 4000;
 dotEnv.config();
-app.use(cors())
+app.use(cors());
 
 app.use('/uploads',express.static('uploads'));
 
